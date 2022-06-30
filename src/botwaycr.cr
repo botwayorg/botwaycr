a62c46286c79d57c2276d86a2a129954e7ec4f94
Abdfn <64256993+abdfnx@users.noreply.github.com>
2022-06-30 23:29:56 +0300

    Update error messages

--- src/botwaycr.cr
+++ src/botwaycr.cr
@@ -2,7 +2,7 @@ require "yaml"
 require "json"
 
 module Botwaycr
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 
   BotwayConfig = JSON.parse(File.read(File.join(Path["~/.botway"].expand(home: true), "botway.json")))
 
@@ -15,7 +15,7 @@ module Botwaycr
 
     def get_token()
       if get_bot_info("lang") != "crystal"
-        raise "ERROR: Bot language is not crystal"
+        raise "ERROR: Your Bot language is not Crystal"
       else
         BotwayConfig["botway"]["bots"][get_bot_info("name")]["bot_token"].to_s
       end
@@ -23,7 +23,7 @@ module Botwaycr
 
     def get_app_id()
       if get_bot_info("lang") != "crystal"
-        raise "ERROR: Bot language is not crystal"
+        raise "ERROR: Your Bot language is not Crystal"
       else
         if get_bot_info("type") == "slack"
           BotwayConfig["botway"]["bots"][get_bot_info("name")]["bot_app_token"].to_s
@@ -35,7 +35,7 @@ module Botwaycr
 
     def get_guild_id(serverName)
       if get_bot_info("lang") != "crystal"
-        raise "ERROR: Bot language is not crystal"
+        raise "ERROR: Your Bot language is not Crystal"
       elsif get_bot_info("type") != "discord"
         raise "ERROR: This function/feature is only working with discord bots."
       else
@@ -45,7 +45,7 @@ module Botwaycr
 
     def get_signing_secret()
       if get_bot_info("lang") != "crystal"
-        raise "ERROR: Bot language is not crystal"
+        raise "ERROR: Your Bot language is not Crystal"
       elsif get_bot_info("type") != "slack"
         raise "ERROR: This function/feature is only working with slack bots."
       else
